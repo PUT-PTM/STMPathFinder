f0f0fcfebe794cccfea4ef6c332df0be9f128986
Piotr Siwinski <siwinski.piotr@outlook.com>
2016-04-03 02:33:06 +0200

    Podłączenie silników z użyciem sterownika, testy silników

--- StmPathFinderCoProj/StmPathFinder_lib/source/PtmPathFinder_gpio.c
+++ StmPathFinderCoProj/StmPathFinder_lib/source/PtmPathFinder_gpio.c
@@ -8,6 +8,9 @@
 #include "stm32f4xx_exti.h"
 #include "misc.h"
 
+
+
+
 /**
  * @brief  Configures the GPIO ports
  * @note	Configures the GPIO D port and sets Pin12..Pin15
@@ -16,7 +19,7 @@
  */
 void LedInit(void)
 {
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOD, ENABLE);
 	GPIO_InitTypeDef GpioLedInit;
 	GpioLedInit.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
 	GpioLedInit.GPIO_Mode = GPIO_Mode_OUT;
@@ -25,6 +28,44 @@ void LedInit(void)
 	GpioLedInit.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOD, &GpioLedInit);
 }
+
+
+/**
+ * @brief  Configures the GPIO ports to run engies on the left side
+ * @note	Configures the GPIO D port and sets Pin9..Pin12
+ * @param 	None
+ * @retval None
+ */
+void LeftEngineInit(void)
+{
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	GPIO_InitTypeDef GpioLedInit;
+	GpioLedInit.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_12;
+	GpioLedInit.GPIO_Mode = GPIO_Mode_OUT;
+	GpioLedInit.GPIO_OType = GPIO_OType_PP;
+	GpioLedInit.GPIO_Speed = GPIO_Speed_100MHz;
+	GpioLedInit.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOD, &GpioLedInit);
+}
+
+/**
+ * @brief  Configures the GPIO ports to run engies on the right side
+ * @note	Configures the GPIO A port and sets Pin10, Pin 13..Pin15
+ * @param 	None
+ * @retval None
+ */
+void RightEngineInit(void)
+{
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	GPIO_InitTypeDef GpioLedInit;
+	GpioLedInit.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	GpioLedInit.GPIO_Mode = GPIO_Mode_OUT;
+	GpioLedInit.GPIO_OType = GPIO_OType_PP;
+	GpioLedInit.GPIO_Speed = GPIO_Speed_100MHz;
+	GpioLedInit.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOA, &GpioLedInit);
+}
+
 
 /**
  * @brief  Configures the user button
@@ -82,7 +123,9 @@ void EXTI0_IRQHandler(void)
 {
 	if (EXTI_GetITStatus(EXTI_Line0) == SET)
 	{
-		GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
+
+		GPIO_ToggleBits(GPIOD, GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_12);
+		GPIO_ToggleBits(GPIOA, GPIO_Pin_10 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     );
 
 		EXTI_ClearITPendingBit(EXTI_Line0);
 	}
--- StmPathFinderCoProj/main.c
+++ StmPathFinderCoProj/main.c
@@ -1,23 +1,39 @@
 #include "PtmPathFinder_gpio.h"
 #include "PtmPathFinder_adc.h"
+#include <math.h>
+#include "stm32f4xx_gpio.h"
 
-
-int AdcResult = 0;
 
 int main(void)
 {
 	SystemInit();
 
-	LedInit();
+	/*Config for engines */
+	LeftEngineInit();
+	RightEngineInit();
+
+	SetupEngines();
+
+	/*Config for the user button*/
 	ButtonInit();
 	ButtonInterruptInit();
-	AdcInit();
 
-	unsigned int i;
+	/*COnfig for Adc */
+	AdcInit();
 
 	while (1)
 	{
-		AdcResult = GetConversionValue();
+
 	}
+
+}
+
+void SetupEngines(void)
+{
+	GPIO_ResetBits(GPIOA, GPIO_Pin_14 | GPIO_Pin_10);
+	GPIO_SetBits(GPIOA, GPIO_Pin_13 | GPIO_Pin_15);
+
+	GPIO_ResetBits(GPIOD, GPIO_Pin_9 | GPIO_Pin_11);
+	GPIO_SetBits(GPIOD, GPIO_Pin_10 | GPIO_Pin_12);
 
 }
