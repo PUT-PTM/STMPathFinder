56c1bccb47f76678e2cd9fd6e7574cfefdbb1be9
Piotr Siwinski <siwinski.piotr@outlook.com>
2016-05-28 16:24:45 +0200

    Poprawki

--- StmPathFinderCoProj/main.c
+++ StmPathFinderCoProj/main.c
@@ -15,40 +15,36 @@ int main(void)
 	{
 		if (mode % 2 == 1)
 		{
-			resultFromFirstAdc = GetConversionValueFromAdc(ADC1);
-			resultFromSecondAdc = GetConversionValueFromAdc(ADC2);
-			voltageFromFirstAdc = resultFromFirstAdc * 3 / 4095;
-			voltageFromSecondAdc = resultFromSecondAdc * 3 / 4095;
-
-			//Test - jesli jest przeszkoda - mrugaj pomaranczowa dioda
-			if (voltageFromFirstAdc > 2 || voltageFromSecondAdc > 2)
-				GPIO_ToggleBits(GPIOD, GPIO_Pin_13);
-
-			if (voltageFromFirstAdc < 2 && voltageFromSecondAdc < 2)
+			if (voltageFromFirstAdc < 1.6 && voltageFromSecondAdc < 1.6)
 			{
 				DriveStraight();
 			}
-
 			//Przeszkoda z lewej strony
-			else if (voltageFromFirstAdc > 2 && voltageFromSecondAdc < 2)
+			else if (voltageFromFirstAdc > 1.6 && voltageFromSecondAdc < 1.6)
 			{
 				GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
-				while (voltageFromFirstAdc > 1.7)
+				while (voltageFromFirstAdc > 1.6)
+				{
 					TurnRight();
+				}
+
+				GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
 			}
 			//Przeszkoda z prawej strony
-			else if (voltageFromFirstAdc < 2 && voltageFromSecondAdc > 2)
+			else if (voltageFromFirstAdc < 1.6 && voltageFromSecondAdc > 1.6)
 			{
-				GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
-				while (voltageFromSecondAdc > 1.7)
+				GPIO_ToggleBits(GPIOD, GPIO_Pin_14);
+				while (voltageFromSecondAdc > 1.6)
+				{
 					TurnLeft();
-			}
-			else
-			{
-				StopVehicle();
+				}
+				GPIO_ToggleBits(GPIOD, GPIO_Pin_14);
 			}
 		}
-
+		else
+		{
+			StopVehicle();
+		}
 
 	}
 
